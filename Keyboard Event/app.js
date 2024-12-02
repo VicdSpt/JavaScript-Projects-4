1ce7bfddaa4cad8346c588c24bc70807a1694b6b
Victor dS <victordespirlet@gmail.com>
2024-12-02 13:14:59 +0100

    Add files via upload

---
+++ Keyboard Event/app.js
@@ -0,0 +1,19 @@
+const useKeyboard = document.getElementById("insert");
+
+window.addEventListener("keydown", () => {
+  useKeyboard.innerHTML = `
+    <div class="key">
+  ${event.key === " " ? "Space" : event.key} 
+  <small>event.key</small>
+</div>
+
+<div class="key">
+  ${event.keyCode}
+  <small>event.keyCode</small>
+</div>
+
+<div class="key">
+  ${event.code}
+  <small>event.code</small>
+</div>`;
+});
